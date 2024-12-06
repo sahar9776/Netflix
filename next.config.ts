e832c02c5031856c5552a766e87c6efce2107155
sahar9776 <sahar.samadi1376@gmail.com>
2024-12-06 10:38:35 +0330

    updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,37 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+
+
+// next.config.mjs
+import path from 'path';
+
+const nextConfig = {
+  webpack(config: { module: { rules: { test: RegExp; use: string[]; }[]; }; resolve: { fallback: any; alias: any; }; }, { isServer }: any) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+
+    // Add custom webpack configurations here
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve('src'),
+    };
+
+    return config;
+  },
+  reactStrictMode: false,
+  typescript: {
+    // Ignore TypeScript errors during the build process
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
